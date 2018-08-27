6988e3aee6b9fb9d766deaa59dd805dd183ee1a7
Jtmichel <jesse@prcapps.com>
2018-08-27 19:45:00 -0400

    data focus page

--- wp-content/themes/sage-8.5.4/page-data-focus.php
+++ wp-content/themes/sage-8.5.4/page-data-focus.php
@@ -4,20 +4,13 @@
  */
 ?>
 <?php while (have_posts()) : the_post(); ?>
-	<?php get_template_part('templates/page', 'header'); ?>
-
-	<!-- <div class="container"> -->
-		<div class="row">
-			<?php //get_template_part('templates/content', 'page-data-focus'); ?>
-
-			<?php $content_arr = get_field('visualizations'); ?>
-			<?php if($content_arr): ?>
-				<?php foreach($content_arr as $content): ?>
-					<?php Roots\Sage\Extras\render_content_grid_item($content); ?>
-				<?php endforeach; ?>
-			<?php endif; ?>
-			<?php get_template_part('templates/flexible-content'); ?>
-		</div>
-	<!-- </div> -->
-
+  <?php get_template_part('templates/page', 'header'); ?>
+  	<div class="page-content-container">
+	  <div class="main-page-content"> 
+	  	<?php get_template_part('templates/flexible-content'); ?>
+	  </div>
+	  <div class="page-sidebar">
+	  	<?php get_template_part('templates/sidebar-content'); ?>
+	  </div>
+	</div>
 <?php endwhile; ?> 
