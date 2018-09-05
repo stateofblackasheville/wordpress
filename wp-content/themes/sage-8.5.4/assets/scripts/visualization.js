af0779d802d09b3c541c0bee48b30605cf73d3d6
Jtmichel <jesse@prcapps.com>
2018-09-05 13:24:31 -0400

    updated visualization.js

--- wp-content/themes/sage-8.5.4/assets/scripts/visualization.js
+++ wp-content/themes/sage-8.5.4/assets/scripts/visualization.js
@@ -1,2 +1,28 @@
-// const domContainer = document.querySelector('#like_button_container');
-// ReactDOM.render(React.createElement(SobaVisualization), domContainer);
+jQuery('.soba-visualization').each(function(){
+	console.log(jQuery(this));
+	var title = jQuery(this).attr('data-title');
+	var spreadsheetID = jQuery(this).attr('data-spreadsheetid');
+	var dataset = jQuery(this).attr('data-dataset');
+	var count = jQuery(this).attr('data-count');
+	var byDate = jQuery(this).attr('data-bydate');
+	var groupBy = jQuery(this).attr('data-groupby');
+	var chartType = jQuery(this).attr('data-charttype');
+
+	console.log('test', title);
+	var props = {
+		title: title, 
+		spreadsheetId: spreadsheetID,
+		dataset: dataset,
+		count: count,
+		byDate: byDate,
+		groupBy: groupBy,
+		chartType: chartType
+	};
+
+	var elm = jQuery(this)[0];
+
+	console.log(elm, props);
+
+
+	ReactDOM.render(React.createElement(SobaVisualization, props), elm);
+});
