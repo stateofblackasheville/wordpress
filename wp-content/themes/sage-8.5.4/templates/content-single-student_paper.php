0c6dd00c5434db10d55eaed84f7955b89ef890ac
Patrick Conant <conantp@prcappss-MacBook-Pro.local>
2018-09-12 18:14:27 -0400

    Student Paper Template

---
+++ wp-content/themes/sage-8.5.4/templates/content-single-student_paper.php
@@ -0,0 +1,21 @@
+<?php while (have_posts()) : the_post(); ?>
+  <article <?php post_class(); ?>>
+    <header>
+      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <?php get_template_part('templates/entry-meta'); ?>
+    </header>
+
+    <div class="entry-content">
+      <?php the_content(); ?>
+      <?php
+      $file = get_field('files');
+
+      echo "<a class='soba-btn' href='".$file['url']."'>Download Student Paper</a><br/><br/>";
+      ?>
+    </div>
+    <footer>
+      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+    </footer>
+    <?php comments_template('/templates/comments.php'); ?>
+  </article>
+<?php endwhile; ?>
