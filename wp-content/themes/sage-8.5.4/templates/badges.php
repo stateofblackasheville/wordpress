af4976f5a2ecd6cf4d6a25f5d312fbdd88a82989
Jtmichel <jesse@prcapps.com>
2018-10-01 17:12:48 -0400

    additional files

---
+++ wp-content/themes/sage-8.5.4/templates/badges.php
@@ -0,0 +1,59 @@
+<?php
+  $related_visualizations = get_posts(array(
+    'post_type' => 'visualization',  
+    'meta_query' => array(
+      array(
+        'key' => 'document_reference', // name of custom field
+        'value' => '"' . get_the_ID() . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+        'compare' => 'LIKE'
+      )
+    )
+  ));
+
+  // data-toggle="tooltip" title="This visualization hasn't been made dynamic yet, but is in the works!"
+
+  // var_dump($related_visualizations);
+
+  if(!empty($related_visualizations)): 
+  	$visualized = true;
+  else:
+  	$visualized = false;
+  endif;	
+?>
+
+<div class="badges">
+	<span class="badge badge--active">
+		Created &nbsp; <ion-icon name="checkmark-circle-outline"></ion-icon>
+	</span>	
+	<ion-icon name="arrow-forward"></ion-icon>
+	<?php if(get_post_type($post_item) == 'student_paper'): ?>
+			<span class="badge <?php if(get_field('index_document', $post_item->ID)): ?>badge--active<?php else: ?>badge--inactive<?php endif; ?>" data-toggle="tooltip"  <?php if(get_field('index_document', $post_item->ID)): ?>title="The index for this student paper has been completed!"<?php else: ?>title="The index for this student paper has not yet been completed."<?php endif; ?>>
+				Indexed
+				<?php if(get_field('index_document', $post_item->ID)): ?> 
+					&nbsp; <ion-icon name="checkmark-circle-outline"></ion-icon>
+				<?php endif; ?>
+			</span>
+			<ion-icon name="arrow-forward"></ion-icon>
+			<span class="badge <?php if($visualized): ?>badge--active<?php else: ?>badge--inactive<?php endif; ?>" data-toggle="tooltip" <?php if($visualized): ?>title="One or more visualizations within this paper have been created."<?php else: ?>title="The visualizations within this paper have not yet been created."<?php endif; ?>>
+				Visualized
+				<?php if($visualized): ?>
+					&nbsp; <ion-icon name="checkmark-circle-outline"></ion-icon> 
+				<?php endif; ?>
+			</span>			
+	<?php endif; ?>
+	<?php if(get_post_type($post_item) == 'visualization'): ?>
+		<span class="badge <?php if(has_post_thumbnail($post_item->ID) || get_field('embed', $post_item->ID)): ?>badge--active<?php endif; ?>" data-toggle="tooltip">
+			Static Visualization
+			<?php if(has_post_thumbnail($post_item->ID) || get_field('embed', $post_item->ID)): ?>
+				&nbsp; <ion-icon name="checkmark-circle-outline"></ion-icon> 
+			<?php endif; ?>
+		</span>
+		<ion-icon name="arrow-forward"></ion-icon>
+		<span class="badge <?php if(get_field('embed', $post_item->ID)): ?>badge--active<?php endif; ?>" data-toggle="tooltip">
+			Dynamic Visualization
+			<?php if(get_field('embed', $post_item->ID)): ?>
+				&nbsp; <ion-icon name="checkmark-circle-outline"></ion-icon> 
+			<?php endif; ?>
+		</span>		
+	<?php endif; ?>	
+</div>
