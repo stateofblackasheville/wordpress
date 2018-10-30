79a81540aebb261f7c3ee1bc71af2f4a699c4a41
Jtmichel <jesse@prcapps.com>
2018-10-29 20:18:41 -0400

    cultural archive

--- wp-content/themes/sage-8.5.4/templates/content-cultural_item.php
+++ wp-content/themes/sage-8.5.4/templates/content-cultural_item.php
@@ -1,44 +1,48 @@
-<article <?php post_class(); ?>>
-	<div class="item-content item-content--cultural-item">
-		<div class="item-content__inner">
-			<?php if(!is_single() && get_post_type() !== 'cultural_item'): ?>
-				<header class="rte rte--georgia">
-					<?php //Roots\Sage\Extras\render_badges(get_post()); ?>
-					<h2 class="entry-title">
-						<a href="<?php the_permalink(); ?>">
-							<?php the_title(); ?>
-						</a>
-					</h2>
-				</header>
+<?php
+
+	$images = get_field('images');
+
+?>
+<div class="item-content item-content--cultural-item">
+	<div class="item-content__inner">
+		<?php if(!is_single() && get_post_type() !== 'cultural_item'): ?>
+			<header class="rte rte--georgia">
+				<?php //Roots\Sage\Extras\render_badges(get_post()); ?>
+				<h2 class="entry-title">
+					<a href="<?php the_permalink(); ?>">
+						<?php the_title(); ?>
+					</a>
+				</h2>
+			</header>
+		<?php endif; ?>
+		<br>
+			<i class="rte rte--georgia rte--large">
+				<?php echo count($images); ?> Images
+			</i>
+		<br>
+		<div class="entry-summary rte rte--georgia">
+			<?php if(is_single() && get_post_type() == 'cultural_item'): ?>
+				<?php the_content(); ?>
+			<?php else: ?>
+				<?php the_excerpt(); ?>
 			<?php endif; ?>
-			<br>
-			<div class="entry-summary rte rte--georgia">
-				<?php if(is_single() && get_post_type() == 'cultural_item'): ?>
-					<?php the_content(); ?>
-				<?php else: ?>
-					<?php the_excerpt(); ?>
-				<?php endif; ?>
-			</div>					
-		    <?php if(is_single() && get_post_type() == 'cultural_item'): ?>
-				<?php 
-
-				$images = get_field('images');
-
-				if( $images ): ?>
-				    <div class="masonry">
-				        <?php foreach( $images as $image ): ?>
-				            <div class="masonry-item">
-				                <a href="<?php echo $image['url']; ?>" data-fancybox="gallery" data-caption="<?php echo $image['caption']; ?>">
-				                     <img src="<?php echo $image['sizes']['square']; ?>" alt="<?php echo $image['alt']; ?>" />
-				                </a>
-				                <?php if(!empty($image['caption'])): ?>
-				                	<p><?php echo $image['caption']; ?></p>
-				                <?php endif; ?>
-				            </div>
-				        <?php endforeach; ?>
-				    </div>
-				<?php endif; ?>
-		    <?php endif; ?>	
-		</div>	
-	</div>
-</article>
+		</div>					
+	    <?php if(is_single() && get_post_type() == 'cultural_item'): ?>
+			<?php 
+			if( $images ): ?>
+			    <div class="masonry">
+			        <?php foreach( $images as $image ): ?>
+			            <div class="masonry-item">
+			                <a href="<?php echo $image['url']; ?>" data-fancybox="gallery" data-caption="<?php echo $image['caption']; ?>">
+			                     <img src="<?php echo $image['sizes']['square']; ?>" alt="<?php echo $image['alt']; ?>" />
+			                </a>
+			                <?php if(!empty($image['caption'])): ?>
+			                	<p><?php echo $image['caption']; ?></p>
+			                <?php endif; ?>
+			            </div>
+			        <?php endforeach; ?>
+			    </div>
+			<?php endif; ?>
+	    <?php endif; ?>	
+	</div>	
+</div>
